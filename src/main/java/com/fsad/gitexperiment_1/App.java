e219bc84edf9b0e251034209480c8446978e4540
Indrani <bhimavarapuindrani2007@gmail.com>
2026-03-04 18:31:41 +0530

    Bug fix update

--- src/main/java/com/fsad/gitexperiment_1/App.java
+++ src/main/java/com/fsad/gitexperiment_1/App.java
@@ -7,7 +7,6 @@ package com.fsad.gitexperiment_1;
 public class App 
 {
     public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    {System.out.println("Bug fix update");
     }
 }
